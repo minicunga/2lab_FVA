414bc1b63a8f1aa64ee83d075fe64240602f85ab
minicunga <77110965+minicunga@users.noreply.github.com>
2021-01-07 19:25:41 +0500

    2lab
    2lab

---
+++ Clock_FVA/MainPage.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Clock_FVA
+{
+    public partial class MainPage : ContentPage
+    {
+       
+            //Общее число квадратов для отражения циферблата часов
+            const int horzDots = 41;
+            const int vertDots = 7;
+            //Задаём трехмерную матрицу показывающую какие квадраты нужно закрасить для какой цифры
+            //идём по строкам сверху вниз 1-активный пиксель
+            static readonly int[,,] numberPatterns = new int[10, 7, 5]
+        {
+            {
+                { 0, 1, 1, 1, 0}, { 1, 0, 0, 0, 1}, { 1, 0, 0, 1, 1}, { 1, 0, 1, 0, 1},
+                { 1, 1, 0, 0, 1}, { 1, 0, 0, 0, 1}, { 0, 1, 1, 1, 0}
+            },
+            {
+                { 0, 0, 1, 0, 0}, { 0, 1, 1, 0, 0}, { 0, 0, 1, 0, 0}, { 0, 0, 1, 0, 0},
+                { 0, 0, 1, 0, 0}, { 0, 0, 1, 0, 0}, { 0, 1, 1, 1, 0}
+            },
+            {
+                { 0, 1, 1, 1, 0}, { 1, 0, 0, 0, 1}, { 0, 0, 0, 0, 1}, { 0, 0, 0, 1, 0},
+                { 0, 0, 1, 0, 0}, { 0, 1, 0, 0, 0}, { 1, 1, 1, 1, 1}
+            },
+            {
+                { 1, 1, 1, 1, 1}, { 0, 0, 0, 1, 0}, { 0, 0, 1, 0, 0}, { 0, 0, 0, 1, 0},
+                { 0, 0, 0, 0, 1}, { 1, 0, 0, 0, 1}, { 0, 1, 1, 1, 0}
+            },
+            {
+                { 0, 0, 0, 1, 0}, { 0, 0, 1, 1, 0}, { 0, 1, 0, 1, 0}, { 1, 0, 0, 1, 0},
+                { 1, 1, 1, 1, 1}, { 0, 0, 0, 1, 0}, { 0, 0, 0, 1, 0}
+            },
+            {
+                { 1, 1, 1, 1, 1}, { 1, 0, 0, 0, 0}, { 1, 1, 1, 1, 0}, { 0, 0, 0, 0, 1},
+                { 0, 0, 0, 0, 1}, { 1, 0, 0, 0, 1}, { 0, 1, 1, 1, 0}
+            },
+            {
+                { 0, 0, 1, 1, 0}, { 0, 1, 0, 0, 0}, { 1, 0, 0, 0, 0}, { 1, 1, 1, 1, 0},
+                { 1, 0, 0, 0, 1}, { 1, 0, 0, 0, 1}, { 0, 1, 1, 1, 0}
+            },
+            {
+                { 1, 1, 1, 1, 1}, { 0, 0, 0, 0, 1}, { 0, 0, 0, 1, 0}, { 0, 0, 1, 0, 0},
+                { 0, 1, 0, 0, 0}, { 0, 1, 0, 0, 0}, { 0, 1, 0, 0, 0}
+            },
+            {
+                { 0, 1, 1, 1, 0}, { 1, 0, 0, 0, 1}, { 1, 0, 0, 0, 1}, { 0, 1, 1, 1, 0},
+                { 1, 0, 0, 0, 1}, { 1, 0, 0, 0, 1}, { 0, 1, 1, 1, 0}
+            },
+            {
+                { 0, 1, 1, 1, 0}, { 1, 0, 0, 0, 1}, { 1, 0, 0, 0, 1}, { 0, 1, 1, 1, 1},
+                { 0, 0, 0, 0, 1}, { 0, 0, 0, 1, 0}, { 0, 1, 1, 0, 0}
+            },
+        };
+
+            //Задаём разделители между блоками цифр :
+            static readonly int[,] colonPattern = new int[7, 2]
+     {
+            { 0, 0 }, { 1, 1 }, { 1, 1 }, { 0, 0 }, { 1, 1 }, { 1, 1 }, { 0, 0 }
+     };
+            //Зададим цвета для активных и не активных пикселей
+            static readonly Color colorOn = Color.Blue;
+            static readonly Color colorOff = new Color(0.5, 0.5, 0.5, 0.25);
+
+            //Зададим элементы для цифр:
+            BoxView[,,] digitBoxViews = new BoxView[6, 7, 5];
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Задаём размеры квадрата
+            double height = 0.85 / vertDots;
+            double width = 0.85 / horzDots;
+
+          
+            double xIncrement = 1.0 / (horzDots - 1);
+            double yIncrement = 1.0 / (vertDots - 1);
+            double x = 0;
+
+            for (int digit = 0; digit < 6; digit++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    double y = 0;
+
+                    for (int row = 0; row < 7; row++)
+                    {
+                        //Создаём элементы boxview построчно
+                        BoxView boxView = new BoxView();
+                        digitBoxViews[digit, row, col] = boxView;
+                        absoluteLayout.Children.Add(boxView,
+                                                    new Rectangle(x, y, width, height),
+                                                    AbsoluteLayoutFlags.All);
+                        y += yIncrement;
+                    }
+                    x += xIncrement;
+                }
+                x += xIncrement;
+
+                // Задаём квадраты разделяющие часы минуты и секунды ::
+                if (digit == 1 || digit == 3)
+                {
+                    int colon = digit / 2;
+
+                    for (int col = 0; col < 2; col++)
+                    {
+                        double y = 0;
+
+                        for (int row = 0; row < 7; row++)
+                        {
+                            // Create the BoxView and set the color.
+                            BoxView boxView = new BoxView
+                            {
+                                Color = colonPattern[row, col] == 1 ?
+                                                colorOn : colorOff
+                            };
+                            absoluteLayout.Children.Add(boxView,
+                                                        new Rectangle(x, y, width, height),
+                                                        AbsoluteLayoutFlags.All);
+                            y += yIncrement;
+                        }
+                        x += xIncrement;
+                    }
+                    x += xIncrement;
+                }
+            }
+
+            // Берём системное время из телефона
+            Device.StartTimer(TimeSpan.FromSeconds(1), OnTimer);
+            OnTimer();
+        }
+
+        //Задаём ф-ю которая меняла бы размеры часов при изменении размеров дисплея
+        void OnPageSizeChanged(object sender, EventArgs args)
+        {
+            absoluteLayout.HeightRequest = vertDots * Width / horzDots;
+        }
+
+        //Задаём активные квадраты в данный момент времени
+        bool OnTimer()
+        {
+            DateTime dateTime = DateTime.Now;
+            int hour = dateTime.Hour;
+            SetDotMatrix(0, hour / 10);
+            SetDotMatrix(1, hour % 10);
+            SetDotMatrix(2, dateTime.Minute / 10);
+            SetDotMatrix(3, dateTime.Minute % 10);
+            SetDotMatrix(4, dateTime.Second / 10);
+            SetDotMatrix(5, dateTime.Second % 10);
+            return true;
+        }
+
+        void SetDotMatrix(int index, int digit)
+        {
+            for (int row = 0; row < 7; row++)
+                for (int col = 0; col < 5; col++)
+                {
+                    bool isOn = numberPatterns[digit, row, col] == 1;
+                    Color color = isOn ? colorOn : colorOff;
+                    digitBoxViews[index, row, col].Color = color;
+                }
+        }
+
+
+    }
+}
